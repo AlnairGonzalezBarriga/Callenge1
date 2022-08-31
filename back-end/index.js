fd9aadc9165f03124ce79a67275529af42106022
alnair gonz <alnair.gonzalez@arkusnexus.com>
2022-08-31 14:11:00 -0600

    Set-up express

--- back-end/index.js
+++ back-end/index.js
@@ -1,1 +1,13 @@
-console.log("Hola mundo");
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.json({
+        ok: true
+    })
+})
+
+app.listen(4000, () =>{
+    console.log("Back-end corriendo");
+});
