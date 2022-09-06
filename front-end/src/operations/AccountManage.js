07b42ce51abe17bc1d0f60f02a3f2c666a25d588
alnair gonz <alnair.gonzalez@arkusnexus.com>
2022-09-06 09:06:16 -0600

    Diseño AccountManage

--- front-end/src/operations/AccountManage.js
+++ front-end/src/operations/AccountManage.js
@@ -1,22 +1,21 @@
 import React, { useContext } from 'react'
 import { AuthContext } from '../auth/context/AuthContext'
 
+import './styles/profileStyles.css'
+
 export const AccountManage = () => {
 
   const { user } = useContext(AuthContext)
 
   return (
-    <div class="container mt-3 mb-4">
-      <h2>Simple Collapsible</h2>
-      <p>Click on the button to toggle between showing and hiding content.</p>
-      <button type="button" class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#demo">Simple collapsible</button>
-      <div id="demo" class="collapse">
-      <div className="card profile-header">
+    <div class="container profile-page mt-3 mb-4">
+      <div class="accordion accordion-flush" id="accordionFlushExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingOne">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+            <div className="card profile-header">
                 <div className="body">
                     <div className="row">
-                        <div className="col-lg-4 col-md-4 col-12">
-                            <div className="profile-image float-md-right"> <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="" /> </div>
-                        </div>
                         <div className="col-lg-8 col-md-8 col-12">
                             <h4 className="m-t-0 m-b-0"><strong>{user.name}</strong></h4>
                             <h5 className="job_post">alnair@gmail.com</h5>
@@ -28,6 +27,12 @@ export const AccountManage = () => {
                     </div>
                 </div>
             </div>
+            </button>
+          </h2>
+          <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+          </div>
+        </div>       
       </div>
     </div>
   )
